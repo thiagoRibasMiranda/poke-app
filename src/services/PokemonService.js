ea32df340358d7082420a2e588380bdc88842705
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-06 10:55:13 -0300

    feat: modify function parameters

--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -1,11 +1,11 @@
-export const getPokemon = (id) => {
-  return fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`).then((res) =>
-    res.json()
+export const getPokemon = (pokemonName) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`).then(
+    (res) => res.json()
   );
 };
 
-export const getPokemonList = () => {
-  return fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(
-    (res) => res.json()
-  );
+export const getPokemonList = (offset) => {
+  return fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=15&offset=${offset}`
+  ).then((res) => res.json());
 };
