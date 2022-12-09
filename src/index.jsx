6eec60fa20e05826ea17a832fa7a600334564442
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-09 09:47:15 -0300

    feat: create the routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Favorites from "./pages/Favorites/Favorites";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
