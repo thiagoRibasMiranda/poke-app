59cfead297db908cea3b33946ce568b2da40caaf
Thiago Ribas <thiagoribasmiranda@hotmail.com>
2022-12-13 14:23:36 -0300

    style: implements display grid to the element Favo

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,17 @@
 import { useState, useEffect } from "react";
+import styled from "styled-components";
+import Header from "../../components/Header/Header";
 import Loading from "../../components/Loading/Loading";
 import PokemonCard from "../../components/PokemonCard/PokemonCard";
+
+const GridPokemonList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-column-gap: 10px;
+  grid-row-gap: 10px;
+  margin: 10px;
+`;
 
 function Favorites() {
   const [favoritesPokemons, setFavoritesPokemons] = useState([]);
@@ -10,16 +21,19 @@ function Favorites() {
   }, []);
   return (
     <div>
-      <h1>Favorites</h1>
-      {favoritesPokemons.length ? (
-        favoritesPokemons.map((pokemon) => (
-          <PokemonCard key={pokemon} pokemonName={pokemon} />
-        ))
-      ) : (
-        <div>
-          <Loading />
-        </div>
-      )}
+      <Header />
+      <h2>Favorites</h2>
+      <GridPokemonList>
+        {favoritesPokemons.length ? (
+          favoritesPokemons.map((pokemon) => (
+            <PokemonCard key={pokemon} pokemonName={pokemon} />
+          ))
+        ) : (
+          <div>
+            <Loading />
+          </div>
+        )}
+      </GridPokemonList>
     </div>
   );
 }
